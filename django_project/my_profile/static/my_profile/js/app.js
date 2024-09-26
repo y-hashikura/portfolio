03e3c9c6ce7fd859615daf11f2a162d4ab8bb5d5
y-hashikura <soudan1031@yahoo.co.jp>
2024-09-27 05:29:47 +0900

    [update] 最新版

--- django_project/my_profile/static/my_profile/js/app.js
+++ django_project/my_profile/static/my_profile/js/app.js
@@ -10,3 +10,23 @@ window.addEventListener('scroll', function() {
         document.body.style.paddingTop = '0';
     }
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelectorAll('.clickable-row').forEach(function(row) {
+        // ダブルクリックイベントを設定
+        row.addEventListener('dblclick', function() {
+            const target = this.dataset.bsTarget;
+            const collapseElement = document.querySelector(target);
+
+            // トグル機能: 開閉状態の確認
+            if (collapseElement.classList.contains('show')) {
+                collapseElement.classList.remove('show');  // 開いているなら閉じる
+            } else {
+                document.querySelectorAll('.collapse').forEach(function(el) {
+                    el.classList.remove('show');  // 他の詳細は閉じる
+                });
+                collapseElement.classList.add('show');  // 対象の詳細を開く
+            }
+        });
+    });
+});
